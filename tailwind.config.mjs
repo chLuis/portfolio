f51b354d06b694d853afbb4f90457a2d2a68c457
Luis Chrestia <chrestia.luis@gmail.com>
2024-03-19 11:27:40 -0300

    add navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,17 @@ export default {
 		extend: {
 			screens: {
 				'xxs': '380px',
+				'xs' : '500px',
 			},
+			animation: {
+				'fade-in' : 'fadein 0.3s ease-in-out'
+			},
+			keyframes: {
+				fadein : {
+					'0%' : {opacity : 0},
+					'100%' : {opacity : 1}
+				}
+			}
 		},
 	},
 	plugins: [],
